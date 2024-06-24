fc636aa359611a8042fe025a74e6d74f3fe76bf9
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-06-24 12:32:07 -0300

    Criacao Projeto

---
+++ src/main/java/com/rodrigo_barbosa/series_filmes_api/SeriesFilmesApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo_barbosa.series_filmes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesFilmesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesFilmesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo_barbosa/series_filmes_api/SeriesFilmesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo_barbosa.series_filmes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesFilmesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
